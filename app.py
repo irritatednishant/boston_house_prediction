5a80366f53eb00cd1a1a868d5b01107c756e54f3
nishant <irritatednishant@gmail.com>
2025-10-23 14:27:19 +0530

    Removed useless column

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-def predict():
+def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -21,6 +21,13 @@ def predict():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input = scalar_model.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regression_model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The house price prediction is: {}".format(output))
 
 if __name__=='__main__':
     app.run(debug=True)
